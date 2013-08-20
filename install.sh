b1712a556bd467bbf810a4e4c353a56bbaa21e1e
Jing Quanqi <jingquanqi@gmail.com>
2013-08-20 09:53:16 +0800

    install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Author:       jingqq5210@gmail.com
+# Created Time: Tue 20 Aug 2013 09:50:58 AM CST
+# FileName:     install.sh
+# Description:  
+# ChangeLog:
+
+cdir=pwd
+ln -s $cdir/.vimrc ~/.vimrc
